d5ecb92192a69d997ba1690cdc9cf794aa2a12fb
jontonsoup4 <jontonsoup4@gmail.com>
2015-11-14 15:29:38 -0600

    added run from command line

--- flagify/flagify.py
+++ flagify/flagify.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from urllib.request import urlopen
 from io import BytesIO
+import sys
 
 
 class Flagify:
@@ -27,3 +28,10 @@ class Flagify:
         flag = Image.open(flag, 'r').convert("RGBA")
         flag = flag.resize((self.width, self.height), Image.BILINEAR)
         return Image.blend(self.picture, flag, 0.5)
+
+
+# optional run flagify.py from command line. First argument is the profile picture, second argument is the flag
+if __name__ == '__main__':
+    picture = sys.argv[1]
+    flag = sys.argv[2]
+    Flagify(picture).add_flag(flag).save(picture.split('.')[0] + '_flagify.png')
